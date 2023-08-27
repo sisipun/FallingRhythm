baa14a61409501137ea81657a14ef1deeb4542f0
sisipun <sisipun@gmail.com>
2023-08-27 13:05:50 +0200

    Level list improve

--- script/interface/popup/levellist/level_list.gd
+++ script/interface/popup/levellist/level_list.gd
@@ -4,8 +4,15 @@ extends VBoxContainer
 
 @export var level_preview_scene: PackedScene
 
+@export_node_path("Button") var _play_button_path: NodePath
+
+@onready var _play_button: Button = get_node(_play_button_path)
+
+var selected_preview: LevelPreview = null 
+
 
 func _ready() -> void:
+	_play_button.pressed.connect(_on_play_button_pressed)
 	for song_timing in SongStorage.song_timings:
 		var level_preview: LevelPreview = level_preview_scene.instantiate()
 		add_child(level_preview)
@@ -14,4 +21,12 @@ func _ready() -> void:
 
 
 func _on_level_pressed(preview: LevelPreview) -> void:
-	EventStorage.emit_signal("level_start_request", preview.song_id)
+	if selected_preview != null:
+		selected_preview.unselect()
+	selected_preview = preview
+	selected_preview.select()
+
+
+func _on_play_button_pressed() -> void:
+	if selected_preview != null:
+		EventStorage.emit_signal("level_start_request", selected_preview.song_id)
--- script/interface/popup/levellist/level_preview.gd
+++ script/interface/popup/levellist/level_preview.gd
@@ -7,9 +7,21 @@ signal pressed
 
 @export_node_path("Label") var _level_name_path: NodePath
 
+@export_color_no_alpha var _selected_color: Color
+@export_color_no_alpha var _unselected_color: Color
+
+# TODO get_theme_stylebox()
+@export var _style: StyleBoxFlat
+
 @onready var _level_name: Label = get_node(_level_name_path)
 
 var song_id: String
+var _override_style: StyleBoxFlat
+
+
+func _ready() -> void:
+	_override_style = _style.duplicate()
+	add_theme_stylebox_override("panel", _override_style)
 
 
 func init(_song_id: String) -> void:
@@ -20,3 +32,11 @@ func init(_song_id: String) -> void:
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenTouch and !event.is_pressed():
 		emit_signal("pressed")
+
+
+func select() -> void:
+	_override_style.bg_color = _selected_color
+
+
+func unselect() -> void:
+	_override_style.bg_color = _unselected_color
